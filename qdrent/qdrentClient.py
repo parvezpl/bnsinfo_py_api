7a88c8e67866b390bf59cac1c71b1e7a759797ca
PARVEZ ALAM <136539319+parvezpl@users.noreply.github.com>
2025-07-21 16:23:49 +0530

    Delete qdrent directory

--- qdrent/qdrentClient.py
+++
@@ -1,7 +0,0 @@
-# utils/helpers.py
-from qdrant_client import QdrantClient
-
-def client(name: str) -> str:
-    # client = QdrantClient(host='localhost', port=6333)
-    client = 11
-    return client
